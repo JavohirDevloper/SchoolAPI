cf1215b711b8673403353841dd8056bbe33d6d02
Javohir Muhammadiyev <138434351+JavohirDevloper@users.noreply.github.com>
2024-09-20 21:08:08 +0500

    prisma

---
+++ prisma/migrations/20240920153956_about/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "About" (
+    "id" SERIAL NOT NULL,
+    "images" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "About_pkey" PRIMARY KEY ("id")
+);
