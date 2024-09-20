796da510b136615a2584bfa2889f38347406ea14
Javohir Muhammadiyev <138434351+JavohirDevloper@users.noreply.github.com>
2024-09-20 21:07:36 +0500

    index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ const handleError = require("./shared/errors/handle");
 
 // const NewsRouter = require("./routers/news.router.js");
 const AdminRouter = require("./routers/admin.router.js");
+const CaruselRouter = require("./routers/carusel.router.js");
+const AboutRouter = require("./routers/about.router.js");
+const GalleryRouter = require("./routers/gallarey.router.js");
 dotenv.config();
 const app = express();
 app.use(cors());
@@ -18,15 +21,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
-app.get("/videos/:filename", (req, res) => {
+app.get("/public/:filename", (req, res) => {
   res.sendFile(
-    path.join(__dirname, "..", "public/videos", req.params.filename)
+    path.join(__dirname, "..", "public/images", req.params.filename)
   );
 });
 
 // routes
 // app.use(NewsRouter);
 app.use(AdminRouter);
+app.use(CaruselRouter);
+app.use(AboutRouter);
+app.use(GalleryRouter);
 connectDB();
 
 // error handle
